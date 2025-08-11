813bffc957f0f7a714835de57573564ba595813a
howardsmann <57996119+howardsmann@users.noreply.github.com>
2025-08-11 23:22:11 +0100

    Update MarketplaceItem.cs

--- Models/MarketplaceItem.cs
+++ Models/MarketplaceItem.cs
@@ -2,6 +2,7 @@ namespace WellbeingHub.Models
 {
     public class MarketplaceItem
     {
+        public string id { get; set; } = System.Guid.NewGuid().ToString();
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
