3dc82aa0afbf40cb85294fa05b474c73f0330064
howardsmann <57996119+howardsmann@users.noreply.github.com>
2025-08-12 11:52:52 +0100

    Update MarketplaceItem.cs

--- Models/MarketplaceItem.cs
+++ Models/MarketplaceItem.cs
@@ -1,9 +1,14 @@
+using System.Text.Json.Serialization;   // <-- add
+
 namespace WellbeingHub.Models
 {
     public class MarketplaceItem
     {
         public string id { get; set; } = System.Guid.NewGuid().ToString();
+
+        [JsonPropertyName("Id")]        // <-- add
         public int Id { get; set; }
+
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
