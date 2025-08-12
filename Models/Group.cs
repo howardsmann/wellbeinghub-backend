75617503f87fc554af6222cc990310a871ad38be
howardsmann <57996119+howardsmann@users.noreply.github.com>
2025-08-12 11:52:27 +0100

    Update Group.cs

--- Models/Group.cs
+++ Models/Group.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;   // <-- add
 
 namespace WellbeingHub.Models
 {
     public class Group
     {
         public string id { get; set; } = System.Guid.NewGuid().ToString();
+
+        [JsonPropertyName("Id")]        // <-- add
         public int Id { get; set; }
+
         public string Name { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
         public List<int> MemberIds { get; set; } = new();
